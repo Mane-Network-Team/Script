76f643a78c6e9a596cc3cb074fa28c390c9cc52d
Mane <mane@manesec.com>
2021-11-10 14:09:07 +0800

    Add files via upload

---
+++ Hightlight Same Goods.js
@@ -0,0 +1,15 @@
+
+var old_img_src = ""
+var find_count = 0
+document.querySelectorAll(".inventory_page > .itemHolder > .item").forEach(elem=>{
+	if (elem.querySelector("img")){
+		var img_obj_src = elem.querySelector("img").src;
+		if (img_obj_src == old_img_src){
+			elem.style.background="#fcff4a";
+			find_count+=1
+		}else{
+			old_img_src = img_obj_src;
+		}
+	}
+});
+alert("[Mane] Find Same object " + find_count.toString() + " items");
